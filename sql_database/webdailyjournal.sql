1689e1aefc13884d3edc6f845f57014c591f0df3
Jonathan Naufal Farrel <jonathannfr2@gmail.com>
2025-01-11 18:57:54 +0700

    Add files via upload

--- sql_database/webdailyjournal.sql
+++ sql_database/webdailyjournal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Jan 2025 pada 12.22
+-- Waktu pembuatan: 11 Jan 2025 pada 12.53
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `web_daily_journal`
+-- Database: `webdailyjournal`
 --
 
 -- --------------------------------------------------------
@@ -41,12 +41,16 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VALUES
-(12, 'Momen Bersejarah!', 'Kami akhirnya mencapai puncak tertinggi, trofi Liga Inggris kini di tangan kami. Perjuangan tanpa lelah, kerja keras, dan dukungan luar biasa dari para penggemar menjadi bahan bakar semangat kami.\r\n\r\nIni bukan sekadar trofi—ini adalah bukti bahwa mimpi besar dapat diraih dengan tekad yang besar. Hari ini, kita semua adalah juara! Terima kasih untuk setiap sorakan, doa, dan kepercayaan. Ini kemenangan kita bersama! ', '20250108124724.jpg', '2025-01-08', 'danny'),
-(13, 'Masa Depan Cerah di Tangan Mereka', 'Hojlund, Garnacho, dan Kobbie Mainoo—trio muda penuh potensi yang membawa harapan baru. Dengan semangat tak kenal takut, energi melimpah, dan bakat luar biasa, mereka menunjukkan bahwa masa depan klub ini berada di tangan yang tepat.\r\n\r\nSetiap langkah mereka di lapangan adalah cerita baru yang menginspirasi, setiap aksi adalah janji untuk kejayaan yang lebih besar. Bersiaplah, dunia, karena generasi ini siap membuat sejarah!', '20250108124736.jpg', '2025-01-08', 'danny'),
-(14, 'Satu Tim, Satu Tujuan', 'Bersama kami berdiri, penuh kebanggaan dan tekad. Squad ini adalah gabungan dari pengalaman, semangat muda, dan dedikasi tanpa batas. Dari para veteran yang memimpin dengan kebijaksanaan, hingga pemain muda yang lapar akan kesuksesan—kami adalah satu keluarga, satu visi.\r\n\r\nMusim ini, kami tidak hanya bermain untuk kemenangan, tetapi untuk lambang di dada, untuk sejarah yang kami tulis bersama.', '20250108124749.jpg', '2025-01-08', 'danny'),
-(15, 'Legenda Sepanjang Masa', 'Ini bukan sekadar squad, ini adalah kumpulan nama-nama yang telah menorehkan sejarah dan membawa kebanggaan tak terhingga. Mereka adalah simbol loyalitas, kerja keras, dan kejayaan yang abadi.\r\n\r\nDari para kiper tak tergantikan, bek kokoh yang jadi tembok pertahanan, gelandang kreatif pengatur irama, hingga penyerang haus gol yang tak kenal ampun—mereka adalah warisan kebanggaan yang tak akan pernah pudar.', '20250108131948.jpg', '2025-01-08', 'danny'),
-(16, 'Theatre of Dreams: Old Trafford', 'Lebih dari sekadar stadion, Old Trafford adalah rumah bagi sejarah, kebanggaan, dan mimpi yang menjadi nyata. Di sinilah generasi pemain telah menciptakan momen-momen magis yang tak terlupakan, dan di sinilah ribuan suara penggemar bersatu untuk mendukung klub tercinta.\r\n\r\nDari kejayaan di masa lalu hingga ambisi untuk masa depan, setiap sudut stadion ini memancarkan jiwa dan semangat Manchester United. Old Trafford adalah simbol dari kebersamaan, kekuatan, dan harapan yang tak pernah padam.', '20250108124930.jpg', '2025-01-08', 'danny'),
-(17, 'Lambang Kebanggaan: Logo Manchester United', 'Lebih dari sekadar simbol, logo ini adalah identitas kami—lambang kejayaan, sejarah, dan tradisi yang tak tergoyahkan. Setiap detailnya mewakili perjalanan panjang, kerja keras, dan semangat tak kenal menyerah yang telah membentuk klub ini.\r\n\r\nDengan warna merah yang menyala, setan ikonik yang menggambarkan keberanian, dan kapal yang melambangkan akar pelabuhan Manchester, logo ini menjadi pengingat bahwa kita adalah satu tim, satu keluarga, satu kebanggaan.', '20250108124951.jpg', '2025-01-08', 'danny');
+(16, 'Intel® Product Compatibility Tool', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111083655.avif', '2025-01-11', 'jonathan'),
+(17, 'Desain dan Pemberdayaan', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111071908.png', '2025-01-11', 'jonathan'),
+(18, 'Penghematan Biaya dan Waktu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111071959.jpg', '2025-01-11', 'jonathan'),
+(19, '64 GB Memori Bandwith Tinggi', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072220.avif', '2025-01-11', 'jonathan'),
+(20, 'Performa yang Lebih Hebat', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072404.jpg', '2025-01-11', 'jonathan'),
+(21, 'Portabilitas yang Mudah', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072454.jpg', '2025-01-11', 'jonathan'),
+(22, 'Keamanan yang Ditingkatkan', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072552.avif', '2025-01-11', 'jonathan'),
+(23, 'Grafis yang Kuat', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072626.png', '2025-01-11', 'jonathan'),
+(24, 'Post by admin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072716.png', '2025-01-11', 'jonathan'),
+(25, 'Pokoe enak pake intel pakkk', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '20250111072736.jpg', '2025-01-11', 'jonathan');
 
 -- --------------------------------------------------------
 
@@ -68,12 +72,15 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`id`, `judul`, `deskripsi`, `gambar`, `tanggal`, `username`) VALUES
-(8, 'trophy', 'momen kemenangan', '20250108123606.jpg', '2025-01-08', 'danny'),
-(9, 'logo', 'manchester united', '20250108124307.jpg', '2025-01-08', 'danny'),
-(10, 'squad', '-', '20250108124325.jpg', '2025-01-08', 'danny'),
-(11, 'trophy', 'momen kemenangan', '20250108124354.jpg', '2025-01-08', 'danny'),
-(12, 'trophy', 'premier league', '20250108124440.jpg', '2025-01-08', 'danny'),
-(13, 'squad', 'all time', '20250108124626.jpg', '2025-01-08', 'danny');
+(24, 'Intel Atom® Processors', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna ', '20250111110345.avif', '2025-01-11', 'admin'),
+(25, 'Intel® Core™ Processors', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111113742.avif', '2025-01-11', 'admin'),
+(26, 'Intel vPro® for Business', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111110603.png', '2025-01-11', 'admin'),
+(27, 'Intel® Xeon® Processors', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111110638.jpg', '2025-01-11', 'admin'),
+(28, 'Intel® Wi-Fi Products', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111110722.png', '2025-01-11', 'admin'),
+(29, 'Intel® Gaudi® AI Accelerators', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111110744.jpg', '2025-01-11', 'admin'),
+(30, 'Intel® Core™ Ultra Processors', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111110822.avif', '2025-01-11', 'admin'),
+(31, 'Intel Atom® Processors', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111113807.jpg', '2025-01-11', 'admin'),
+(32, 'Intel® Core™ Processors', 'Lorem ipsum dolor sit amet, consectetur adipiscing agna', '20250111113832.png', '2025-01-11', 'admin');
 
 -- --------------------------------------------------------
 
@@ -93,16 +100,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
-(26, 'danny', '21232f297a57a5a743894a0e4a801fc3', '20250108123734.jpg'),
-(28, 'sulthan', '21232f297a57a5a743894a0e4a801fc3', '20250108124117.jpg'),
-(29, 'yustr', '21232f297a57a5a743894a0e4a801fc3', '20250108130656.jpg'),
-(30, 'agus', '21232f297a57a5a743894a0e4a801fc3', '20250108130720.jpg'),
-(31, 'tio', '21232f297a57a5a743894a0e4a801fc3', '20250108130745.jpg'),
-(32, 'alex', '21232f297a57a5a743894a0e4a801fc3', '20250108130809.jpg'),
-(33, 'riski', '21232f297a57a5a743894a0e4a801fc3', '20250108130828.jpg'),
-(34, 'davi', '21232f297a57a5a743894a0e4a801fc3', '20250108130849.jpg'),
-(35, 'djiwo', '21232f297a57a5a743894a0e4a801fc3', '20250108130913.jpg'),
-(36, 'legowo', '21232f297a57a5a743894a0e4a801fc3', '20250108130949.jpg');
+(24, 'admin', 'd41d8cd98f00b204e9800998ecf8427e', '20250111122456.jpg'),
+(25, 'danny', '21232f297a57a5a743894a0e4a801fc3', '20250111122434.jpg'),
+(27, 'jonathan', 'd41d8cd98f00b204e9800998ecf8427e', '20250111122448.png'),
+(28, 'user', '21232f297a57a5a743894a0e4a801fc3', '20250111122513.jpg'),
+(29, 'nathan', '21232f297a57a5a743894a0e4a801fc3', '20250111122533.jpeg'),
+(30, 'danny', '21232f297a57a5a743894a0e4a801fc3', '20250111122611.jpg'),
+(31, 'pusat', '21232f297a57a5a743894a0e4a801fc3', '20250111122638.avif');
 
 --
 -- Indexes for dumped tables
@@ -134,19 +138,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT untuk tabel `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
